5417477d753520fe7df05d4b55ae4974c855df17
Arushi Prakash <arushiprakash@hotmail.com>
2017-10-17 16:16:26 -0400

    node does not work, module not displaying

---
+++ app/public/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Arushi',
+    profilePic: 'https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/22096162_10209551909705417_6069377205119052405_o.jpg?oh=158c0c5d1a9c54be9d0d0c50909162a7&oe=5A3A0A0D',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
